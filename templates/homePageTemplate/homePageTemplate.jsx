28406ab8fc29b201077bc56540222511be7b01e5
nikolay.pylypchatin <nikolay.pylypchatin@gmail.com>
2020-07-10 16:13:47 +0300

    fix

--- templates/homePageTemplate/homePageTemplate.jsx
+++ templates/homePageTemplate/homePageTemplate.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import styles from './homePageTemplate.module.scss';
 import * as actionCreated from '../../store/actions/counter';
-import * as actionType from '../../store/actions/actionTypes';
 import { connect } from 'react-redux';
 
 const homePageTemplate = (props) => {
-    console.log(actionType);
+    console.log(actionCreated);
     return (
         <div className={styles.homePageTemplate}>
             <button onClick={() => props.increment()}>+</button>
@@ -21,8 +20,8 @@ export default connect(
     },
     (dispatch) =>{
         return {
-            increment: () => dispatch({type:actionType.COUNTER_INCRIMENT}),
-            decrement: () => dispatch({type:actionType.COUNTER_DECRIMENT}),
+            increment: () => dispatch(actionCreated.counterIncriment()),
+            decrement: () => dispatch(actionCreated.counterDecriment()),
         }
     }
 )(homePageTemplate);
